ccf70169b2b0f0af0954c7937e4c04db7a5a1851
Valentina15-lopez <valentina.lopez@despegar.com>
2024-03-01 19:48:26 -0300

    cambios

--- client/src/components/SocketManager.jsx
+++ client/src/components/SocketManager.jsx
@@ -1,12 +1,75 @@
 import { atom, useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
+import Peer from "simple-peer";
 
 export const socket = io("http://localhost:3001");
 export const avatarAtom = atom([]);
 
-export const SocketManager = () => {
+export const SocketManager = (
+  setStream,
+  stream,
+  myVideo,
+  userVideo,
+  connectionRef,
+  setCallAccepted,
+  setCallEnded
+) => {
   const [avatar, setAvatar] = useAtom(avatarAtom);
+  const [call, setCall] = useState({});
+  const [me, setMe] = useState("");
+  const [name, setName] = useState("");
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: call.from });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userVideo.current = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
+
+  const callUser = (id) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name,
+      });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userVideo.current = currentStream;
+    });
+
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    connectionRef.current.destroy();
+
+    window.location.reload();
+  };
   useEffect(() => {
     function onConnect() {
       console.log("connected");
@@ -22,6 +85,11 @@ export const SocketManager = () => {
     function onCharacters(value) {
       setAvatar(value);
     }
+    socket.on("me", (id) => setMe(id));
+
+    socket.on("callUser", ({ from, name: callerName, signal }) => {
+      setCall({ isReceivingCall: true, from, name: callerName, signal });
+    });
 
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
--- client/src/pages/AulaVirtual/index.jsx
+++ client/src/pages/AulaVirtual/index.jsx
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import AulaScene from "../../components/AulaScene";
 import { Canvas } from "@react-three/fiber";
 import { SocketManager } from "../../components/SocketManager";
+import { Grid, Paper, makeStyles } from "@material-ui/core";
+import { SocketContext } from "../../components/SocketManager";
 
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: "550px",
+    [theme.breakpoints.down("xs")]: {
+      width: "300px",
+    },
+  },
+  gridContainer: {
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  paper: {
+    padding: "10px",
+    border: "2px solid black",
+    margin: "10px",
+  },
+}));
 const AulaVirtual = () => {
+  const classes = useStyles();
+  const [stream, setStream] = useState();
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const [callEnded, setCallEnded] = useState(false);
+  const [callAccepted, setCallAccepted] = useState(false);
+
+  const connectionRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+
+        myVideo.current = currentStream;
+      });
+  }, []);
   return (
     <div>
       <h1>Udeverso - Aula Virtual</h1>
-      <SocketManager />
+      <SocketManager
+        setStream
+        stream
+        myVideo
+        userVideo
+        connectionRef
+        setCallAccepted
+        setCallEnded
+      />
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
       <Canvas shadows camera={{ position: [8, 8, 8], fov: 30 }}>
         <AulaScene />
       </Canvas>
