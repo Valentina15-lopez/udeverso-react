c51337db3a6bee189f86ee12d97719432d9664a7
Mathias Nieres <mathiasnieres@gmail.com>
2024-04-20 21:00:32 -0300

    fix: corrijo el listar usuarios

--- client/src/components/Administrador/UserList.jsx
+++ client/src/components/Administrador/UserList.jsx
@@ -1,28 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { userAtom } from "../../components/ContexProvider";
 import { useAtom } from "jotai";
 
 const UserList = ({ onUserClick }) => {
-  const [users] = useAtom(userAtom);
-  console.log(users);
-  console.log("usernames", users);
-  const handleUserClick = (userId) => {
-    // Llama a la función de devolución de llamada onUserClick con el ID del usuario
-    onUserClick(userId);
-  };
-  return (
-    <div>
-      <h2>Lista de Usuarios</h2>
-      <ul>
-        {users &&
-          users.map((user) => (
-            <li key={user.id} onClick={() => handleUserClick(user.id)}>
-              {user.id}
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
+    // Estado para almacenar la lista de usuarios
+    const [users, setUsers] = useState([]);
+
+    // Cargar la lista de usuarios cuando el componente se monta
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get("http://localhost:3001/api/users");
+                setUsers(response.data); // Asignar la lista de usuarios al estado
+            } catch (error) {
+                console.error("Error al obtener la lista de usuarios:", error);
+            }
+        };
+
+        fetchUsers(); // Llamar a la función para cargar los usuarios
+    }, []); // Efecto sin dependencias para que se ejecute solo una vez al montar el componente
+
+    const handleUserClick = (userId) => {
+        // Llama a la función de devolución de llamada onUserClick con el ID del usuario
+        onUserClick(userId);
+    };
+
+    return (
+        <div>
+            <h2>Lista de Usuarios</h2>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.usuario} onClick={() => handleUserClick(user.usuario)}>
+                        {user.usuario} {/* Mostrar el nombre del usuario */}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default UserList;
