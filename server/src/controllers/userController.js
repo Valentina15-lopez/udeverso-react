85103fa2891f8afbdde7909dfb365faf85ced5f5
Mathias Nieres <mathiasnieres@gmail.com>
2024-04-27 20:18:40 -0300

    fix: saco codigo comentado

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -58,28 +58,6 @@ export const login = async (req, res) => {
     }
 };
 
-/*
-export const createUser = async (req, res) => {
-    console.log("Se llamo al endpoint POST /api/users con " + JSON.stringify(req.body));
-    const { usuario, contrasenia, nombre_para_mostrar, avatar_id, correo, es_estudiante } = req.body;
-
-    try {
-        const saltRounds = 10;
-        const hashContrasenia = await bcrypt.hash(contrasenia, saltRounds);
-
-        const nuevoUsuario = await pool.query(
-            "INSERT INTO users (usuario, contrasenia, nombre_para_mostrar, avatar_id, correo, es_estudiante) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-            [usuario, hashContrasenia, nombre_para_mostrar, avatar_id, correo, es_estudiante]
-        );
-
-        res.json(nuevoUsuario.rows[0]);
-    } catch (error) {
-        console.error("Error al crear usuario:", error);
-        res.status(500).send("Error interno del servidor");
-    }
-};
-*/
-
 export const createUser = async (req, res) => {
     console.log("Se llamó al endpoint POST /api/users con " + JSON.stringify(req.body));
     const { usuario, contrasenia, nombre_para_mostrar, avatar_id, correo, es_estudiante } = req.body;
@@ -105,19 +83,6 @@ export const createUser = async (req, res) => {
     }
 };
 
-/*
-export const getAllUsers= async (req, res) => {
-    console.log("Se llamo al endpoint GET /api/users con " + JSON.stringify(req.body));
-    try {
-        const users = await pool.query("SELECT * FROM users");
-        res.status(200).json(users.rows);
-        //console.dir("Los usuarios devueltos son " + users);
-    } catch (error) {
-        res.status(500).json({ error: "Error al obtener usuarios" });
-    }
-}
-*/
-
 export const getAllUsers = async (req, res) => {
     console.log("Se llamó al endpoint GET /api/users con " + JSON.stringify(req.body));
 
@@ -137,28 +102,6 @@ export const getAllUsers = async (req, res) => {
         res.status(500).json({ message: "Error interno del servidor" });
     }
 };
-
-/*
-export const getUser = async (req,res)=>{
-    console.log("Se llamo al endpoint GET /api/users/:id con " + JSON.stringify(req.body));
-    try {
-        const id = req.params.id;
-        const users = await pool.query("SELECT * FROM users WHERE usuario = $1", [id]);
-
-        //console.dir("Los usuarios devueltos son " + users.rows.json);
-
-        // Verificamos si se encontró el usuario
-        if (users.rows.length > 0) {
-            res.status(200).json(users.rows[0]); // Si el usuario existe, devolvemos sus datos
-        } else {
-            res.status(404).send("Usuario no encontrado"); // Si no existe, devolvemos un error 404
-        }
-    } catch (error) {
-        console.error("Error al obtener usuarios:", error.message); // Imprimir el mensaje de error en la consola
-        res.status(500).json({ error: "Error al obtener el usuario." }); // Devolver un mensaje de error al cliente
-    }
-}
- */
 
 export const getUser = async (req, res) => {
     console.log("Se llamó al endpoint GET /api/users/:id con " + JSON.stringify(req.body));
@@ -182,74 +125,6 @@ export const getUser = async (req, res) => {
         res.status(500).json({ message: "Error interno del servidor" });  // Responder con un error 500
     }
 };
-
-/*
-export const updateUser = async(req,res)=>{
-    console.log("Se llamo al endpoint PUT /api/users/:usuario con " + JSON.stringify(req.body));
-    try {
-        const { usuario } = req.params; // El usuario a actualizar
-        const {
-            nombre_para_mostrar,
-            avatar_id,
-            correo,
-            es_estudiante,
-        } = req.body;
-
-        const updateFields = [];
-        const updateValues = [usuario]; // Comienza con el usuario para la cláusula WHERE
-        let updateIndex = 2; // El primer índice libre para parámetros dinámicos
-
-        // Agregar cada campo solo si está definido en el cuerpo de la solicitud
-        if (nombre_para_mostrar !== undefined) {
-            updateFields.push(`nombre_para_mostrar = $${updateIndex}`);
-            updateValues.push(nombre_para_mostrar);
-            updateIndex++;
-        }
-
-        if (avatar_id !== undefined) {
-            updateFields.push(`avatar_id = $${updateIndex}`);
-            updateValues.push(avatar_id);
-            updateIndex++;
-        }
-
-        if (correo !== undefined) {
-            updateFields.push(`correo = $${updateIndex}`);
-            updateValues.push(correo);
-            updateIndex++;
-        }
-
-        if (es_estudiante !== undefined) {
-            // Asegúrate de manejar correctamente el tipo de datos para `es_estudiante`
-            const esEstudianteBoolean = es_estudiante === "1" || es_estudiante === "true";
-            updateFields.push(`es_estudiante = $${updateIndex}`);
-            updateValues.push(esEstudianteBoolean);
-            updateIndex++;
-        }
-
-        if (updateFields.length === 0) {
-            return res.status(400).json({ message: "Nada para actualizar" });
-        }
-
-        const updateUserQuery = `
-      UPDATE users
-      SET ${updateFields.join(", ")}
-      WHERE usuario = $1
-        RETURNING *;
-    `;
-
-        const updatedUser = await pool.query(updateUserQuery, updateValues);
-
-        if (updatedUser.rowCount === 0) {
-            return res.status(404).send("Usuario no encontrado");
-        }
-
-        res.json(updatedUser.rows[0]); // Devuelve el usuario actualizado
-    } catch (err) {
-        console.error("Error actualizando usuario:", err.message);
-        res.status(500).send("Error del servidor");
-    }
-}
-*/
 
 export const updateUser = async (req, res) => {
     console.log("Se llamó al endpoint PUT /api/users/:usuario con " + JSON.stringify(req.body));
@@ -304,29 +179,6 @@ export const updateUser = async (req, res) => {
     }
 };
 
-/*
-export const deleteUser = async(req,res)=> {
-    console.log("Se llamo al endpoint DELETE /api/users/:usuario con " + JSON.stringify(req.body));
-    try {
-        const { usuario } = req.params; // ID del usuario a eliminar
-
-        console.log("El usuario a borrar es " + usuario);
-
-        // Ejecutar la operación DELETE
-        const result = await pool.query("DELETE FROM users WHERE usuario = $1", [usuario]);
-
-        // Verificar cuántas filas fueron afectadas
-        if (result.rowCount > 0) {
-            res.status(200).send("Usuario borrado con éxito");
-        } else {
-            res.status(404).send("Usuario no encontrado");
-        }
-    } catch (error) {
-        console.error("Error al eliminar usuario:", error);
-        res.status(500).send("Error del servidor");
-    }
-}
-*/
 export const deleteUser = async (req, res) => {
     console.log("Se llamó al endpoint DELETE /api/users/:usuario con " + JSON.stringify(req.body));
 
--- server/src/controllers/usersMaterialsController.js
+++ server/src/controllers/usersMaterialsController.js
@@ -1,28 +1,5 @@
 import {UsuariosMateriales} from "../models/index.js";
 
-
-/*
-export const addMaterialToUser = async (req, res)=>{
-    console.log("Se llamo al endpoint POST /api/users/material con " + JSON.stringify(req.body));
-    try {
-        const { usuario, nombre, ext } = req.body;
-        const fileBuffer = req.file.buffer;
-
-        // Guardar el archivo en la base de datos PostgreSQL
-        const result = await pool.query(
-            "INSERT INTO usuario_material (usuario, nombre, ext, material) VALUES ($1, $2, $3, $4)",
-            [usuario, nombre, ext, fileBuffer]
-        );
-
-        console.log("Material agregado exitosamente");
-        res.status(200).send("Material agregado exitosamente");
-    } catch (error) {
-        console.error("Error al agregar material:", error.message);
-        res.status(500).send("Error al agregar material");
-    }
-}
-*/
-
 export const addMaterialToUser = async (req, res) => {
     console.log("Se llamó al endpoint POST /api/users/material con " + JSON.stringify(req.body));
 
@@ -46,22 +23,6 @@ export const addMaterialToUser = async (req, res) => {
     }
 };
 
-/*
-export const getAllMaterialsOfUser= async (req,res)=> {
-    console.log("Se llamo al endpoint GET /api/users/:usuario/material con " + JSON.stringify(req.body));
-    try {
-        const { usuario } = req.params;
-        const query = "SELECT * FROM usuario_material WHERE usuario = $1";
-        const values = [usuario];
-        const { rows } =  await pool.query(query, values);
-        const materiales = await rows;
-        res.status(200).json(materiales);
-    } catch (error) {
-        res.status(500).json({ error: "Error al obtener usuarios" });
-    }
-}
-*/
-
 export const getAllMaterialsOfUser = async (req, res) => {
     console.log("Se llamó al endpoint GET /api/users/:usuario/material con " + JSON.stringify(req.body));
 
@@ -79,31 +40,6 @@ export const getAllMaterialsOfUser = async (req, res) => {
         res.status(500).json({ message: "Error al obtener materiales del usuario" });  // Manejo de errores
     }
 };
-
-/*
-export const deleteMaterialOfUser = async (req, res) => {
-    console.log("Se llamo al endpoint DELETE /api/users/:usuario/material/:nombre con " + JSON.stringify(req.body));
-    try {
-        const { usuario , nombre} = req.params; // ID del usuario a eliminar
-
-        console.log("El material " + nombre + " se va a borrar del usuario " + usuario);
-
-        // Ejecutar la operación DELETE
-        const result = await pool.query("DELETE FROM usuario_material WHERE usuario = $1 and nombre = $2", [usuario, nombre]);
-
-        // Verificar cuántas filas fueron afectadas
-        if (result.rowCount > 0) {
-            console.log("Material borrado con exito");
-            res.status(200).send("Material borrado con exito");
-        } else {
-            console.log("Material no encontrado");
-            res.status(404).send("Material no encontrado");
-        }
-    } catch (error) {
-        console.error("Error al eliminar material:", error);
-        res.status(500).send("Error del servidor");
-    }
-}*/
 
 export const deleteMaterialOfUser = async (req, res) => {
     console.log("Se llamó al endpoint DELETE /api/users/:usuario/material/:nombre con " + JSON.stringify(req.body));
