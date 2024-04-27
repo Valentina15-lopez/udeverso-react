e48a400a0fefb69cff8cfa9fde7e6499ac67dfa1
Mathias Nieres <mathiasnieres@gmail.com>
2024-04-27 14:46:12 -0300

    fix: corrijo error en el modelo al agregar un material

--- server/src/models/UsuariosMateriales.js
+++ server/src/models/UsuariosMateriales.js
@@ -1,36 +1,31 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
-import {Usuarios} from './index.js';
+import Usuarios from './Usuarios.js';
 
 const UsuariosMateriales = sequelize.define('UsuariosMateriales', {
     usuario: {
         type: DataTypes.STRING,
-        allowNull: false,  // No se permite nulo
+        allowNull: false,
         references: {
             model: Usuarios,  // Referencia al modelo Usuario
-            key: 'usuario',  // Clave foránea apunta a `usuario` en `Usuario`
+            key: 'usuario',
         },
+        primaryKey: true,  // Parte de la clave primaria compuesta
     },
     nombre: {
         type: DataTypes.STRING,
-        allowNull: false,  // No se permite nulo
+        allowNull: false,
+        primaryKey: true,  // Parte de la clave primaria compuesta
     },
     ext: {
         type: DataTypes.STRING,
     },
     material: {
-        type: DataTypes.BLOB,  // Almacena datos binarios (por ejemplo, archivos)
+        type: DataTypes.BLOB,  // Datos binarios
     },
-    // Clave primaria compuesta por `usuario` y `nombre`
 }, {
-    tableName: 'usuario_material',  // Nombre de la tabla en la base de datos
-    timestamps: true,  // Permite que Sequelize maneje `createdAt` y `updatedAt`
-    indexes: [
-        {
-            unique: true,
-            fields: ['usuario', 'nombre'],  // Clave primaria compuesta
-        }
-    ],
+    tableName: 'usuario_material',
+    timestamps: true,
 });
 
 export default UsuariosMateriales;
