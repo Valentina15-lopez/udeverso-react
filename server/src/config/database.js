0f35c088f452372c982e18d92d58c66d74d81a53
Mathias Nieres <mathiasnieres@gmail.com>
2024-04-30 14:04:38 -0300

    feature: agrego configuraciones para pruebas

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,11 +1,40 @@
+import dotenv from 'dotenv';
+dotenv.config();  // Cargar variables de entorno desde .env
+
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('udeverso', 'udeverso_user','puerta2024',{
-    host : 'localhost',dialect: "postgres",logging: false});
+const databaseConfig = {
+    test: {
+        username: process.env.TEST_DB_USER,
+        password: process.env.TEST_DB_PASS,
+        database: process.env.TEST_DB_NAME,
+        host: process.env.TEST_DB_HOST,
+        dialect: 'postgres',  // O el dialecto de tu base de datos
+        logging: false,  // No registrar SQL en el entorno de pruebas
+    },
+    production: {
+        username: process.env.PROD_DB_USER,
+        password: process.env.PROD_DB_PASS,
+        database: process.env.PROD_DB_NAME,
+        host: process.env.PROD_DB_HOST,
+        dialect: 'postgres',  // Cambia según tu base de datos de producción
+        logging: false,  // Generalmente, no se registran operaciones en producción
+        // Otras configuraciones para producción, como SSL
+    },
+};
 
-sequelize.authenticate().
-then(()=>{console.log("Conexión a la base de datos exitosa");})
-    .catch((error)=>{console.log("Error al conectar a la base de datos:", error)});
+const currentEnvironment = process.env.NODE_ENV || 'test';  // Si no está definido, por defecto es pruebas
+const sequelizeConfig = databaseConfig[currentEnvironment];  // Elegir configuración según entorno
 
-//module.exports = sequelize
-export default sequelize;
+const sequelize = new Sequelize(
+    sequelizeConfig.database,
+    sequelizeConfig.username,
+    sequelizeConfig.password,
+    {
+        host: sequelizeConfig.host,
+        dialect: sequelizeConfig.dialect,
+        logging: sequelizeConfig.logging,
+    }
+);
+
+export default sequelize;  // Exportar la instancia de Sequelize
