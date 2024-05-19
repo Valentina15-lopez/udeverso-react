6944887dd867a688dc149d68d261e1a7e9bc91f5
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-19 17:23:21 -0300

    videoscreen

---
+++ client/src/components/Streaming/VideoScreen.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from "react";
+
+export const VideoScreen = ({ stream }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  return (
+    <>
+      <video
+        data-testid="peer-video"
+        style={{ width: "100%" }}
+        ref={videoRef}
+        autoPlay
+      />
+    </>
+  );
+};
