eaf7f2c5899e801bb0dc758bd09d4c613a0e3340
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-24 14:52:39 -0300

    arreglo video

--- client/src/components/Streaming/room.jsx
+++ client/src/components/Streaming/room.jsx
@@ -41,7 +41,11 @@ export const Room = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex grow">
-        <div className={`grid gap-4 `}>
+        <div
+          className={`grid gap-4 ${
+            screenSharingVideo ? "w-1/5 grid-cols-2" : "grid-cols-3"
+          }`}
+        >
           {screenSharingId !== userId && (
             <div>
               <VideoPlayer stream={stream} isOwnStream={true} />
@@ -60,6 +64,11 @@ export const Room = () => {
         </div>
         {chat.isChatOpen && <Chat />}
       </div>
+      {screenSharingVideo && (
+        <div className="w-4/5 pr-4">
+          <VideoPlayer stream={screenSharingVideo} />
+        </div>
+      )}
       <div className="fixed bottom-0 right-0 mr-16 mb-16 gap-3">
         <div className="flex gap-3">
           <ShareScreenButton onClick={shareScreen} />
