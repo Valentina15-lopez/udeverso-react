66c22d291e3ef70e3323d4cc2f4013c5967ae860
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-26 14:43:33 -0300

    movimienot

--- client/src/components/AulaScene.jsx
+++ client/src/components/AulaScene.jsx
@@ -61,7 +61,7 @@ const AulaScene = () => {
           {(texture) => (
             <mesh
               receiveShadow
-              position={[0, 0, 0]}
+              position={[0, -0.467, 0]}
               rotation={[-Math.PI / 2, 0, 0]}
               scale={0.02}
               geometry={gltf.nodes.PisoAula.geometry}
