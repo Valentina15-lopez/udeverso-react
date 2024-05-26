7af9e7aca212237f983ab9e161ccc7099416f94f
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-26 15:15:41 -0300

    movimienot

--- client/src/components/AulaScene.jsx
+++ client/src/components/AulaScene.jsx
@@ -45,7 +45,7 @@ const AulaScene = () => {
               receiveShadow
               geometry={gltf.nodes.PisoAula.geometry}
               rotation-x={-Math.PI / 2}
-              position={[0, -0.467, 0]}
+              position-y={-0.001}
               onClick={(e) => socket.emit("move", [e.point.x, 0, e.point.z])}
               onPointerEnter={() => setOnFloor(true)}
               onPointerLeave={() => setOnFloor(false)}
