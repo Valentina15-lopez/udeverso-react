fd9c7ff61128dec2657eeedd75e433860b64fd8e
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 09:37:32 -0300

    merge

--- client/src/components/Streaming/UploadButton.jsx
+++ client/src/components/Streaming/UploadButton.jsx
@@ -1,24 +1,35 @@
-import React, { useContext, useState } from "react";
+import React, { useRef, useContext } from "react";
 import { RoomContext } from "../../context/RoomContext";
 import { Button } from "../../common/Button";
 
 export const UploadButton = () => {
   const { uploadFile } = useContext(RoomContext);
-  const [isSharing, setIsSharing] = useState(false);
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
       uploadFile(file);
-      setIsSharing(!isSharing);
     }
   };
 
   return (
     <div>
-      <Button className="p-4 mx-2" onClick={handleFileChange} type="file">
-        {isSharing ? "Detener compartición" : "Compartir Material"}
+      <Button onClick={handleButtonClick} type="button">
+        Compartir Material
       </Button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
     </div>
   );
 };
