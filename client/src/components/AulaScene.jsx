cb1ec76c3309e8424cfe0eccff6bb7cae50aeeb9
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 01:11:21 -0300

    merge

--- client/src/components/AulaScene.jsx
+++ client/src/components/AulaScene.jsx
@@ -9,7 +9,7 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RoomContext } from "../context/RoomContext";
 import { UserContext } from "../context/UserContext";
-import modeloGlb from "../assets/modeloAula3.glb";
+import modeloGlb from "../assets/Aula.glb";
 import { VideoScreen } from "../components/Streaming/VideoScreen";
 import { Pizarron } from "./Pizarron";
 
