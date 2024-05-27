c2cb8ca268a14e2abf0ab9c743976bc25326ddcb
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 09:33:55 -0300

    merge

--- client/src/components/Streaming/UploadButton.jsx
+++ client/src/components/Streaming/UploadButton.jsx
@@ -17,9 +17,9 @@ export const UploadButton = () => {
   return (
     <div>
       <Button className="p-4 mx-2" onClick={handleFileChange}>
+        <input type="file" />
         {isSharing ? "Detener compartición" : "Compartir Material"}
       </Button>
-      <input type="file" onChange={handleFileChange} />
     </div>
   );
 };
