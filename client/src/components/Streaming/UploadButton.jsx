1fd9110791e3fee91387110bde6dac3ecefc9e99
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 01:20:23 -0300

    merge

---
+++ client/src/components/Streaming/UploadButton.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { RoomContext } from "../context/RoomContext";
+
+export const UploadButton = () => {
+  const { uploadFile } = useContext(RoomContext);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      uploadFile(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+    </div>
+  );
+};
