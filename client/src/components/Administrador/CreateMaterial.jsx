2435aed15175b0bf14d43f5ae1c5843cbe2e61bb
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 09:20:37 -0300

    merge

--- client/src/components/Administrador/CreateMaterial.jsx
+++ client/src/components/Administrador/CreateMaterial.jsx
@@ -16,7 +16,7 @@ const CreateMaterial = () => {
       try {
         // const response = await axios.get("http://localhost:3001/api/users");
         const response = await axios.get(
-          "https://metaversoude2.ddns.net:/api/users"
+          "https://metaversoude2.ddns.net:3001/api/users"
         );
         const users = response.data; // Supongamos que aquí tienes una lista de usuarios
         setUsuarios(users); // Guardar la lista de usuarios en el estado
