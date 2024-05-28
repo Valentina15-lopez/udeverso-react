01c6bd426aacf5fd9eef62f62979c4a386d95be9
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 21:20:11 -0300

    agrego rol

--- client/src/components/Streaming/UploadButton.jsx
+++ client/src/components/Streaming/UploadButton.jsx
@@ -41,7 +41,7 @@ export const UploadButton = () => {
           {materiales.map((material) => (
             <div key={material.id}>
               <Button onClick={() => selectMaterial(material.path)}>
-                <span>{material.name}</span>
+                <span>{material.id.name}</span>
               </Button>
             </div>
           ))}
