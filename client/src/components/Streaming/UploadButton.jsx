38d31a2c6b1b044c475fe25fa58c758e2592f7a8
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 21:32:51 -0300

    agrego rol

--- client/src/components/Streaming/UploadButton.jsx
+++ client/src/components/Streaming/UploadButton.jsx
@@ -1,8 +1,7 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { RoomContext } from "../../context/RoomContext";
 import { Button } from "../../common/Button";
 import axios from "axios";
-import * as THREE from "three";
 import { Modal } from "../../common/Modal";
 import { UserContext } from "../../context/UserContext";
 
@@ -11,11 +10,13 @@ export const UploadButton = () => {
   const [materiales, setMateriales] = useState([]);
   const { userName } = useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const closeModal = () => setIsModalOpen(false);
 
   const openModal = async () => {
     setIsModalOpen(true);
+    setLoading(true); // Iniciar el estado de carga
     try {
       const response = await axios.get(
         `https://metaversoude2.ddns.net:3001/api/users/${userName}/material`
@@ -25,11 +26,14 @@ export const UploadButton = () => {
     } catch (error) {
       console.error("Error al cargar materiales del usuario:", error);
     }
+    setLoading(false); // Terminar el estado de carga
   };
+
   const selectMaterial = (materialPath) => {
     setFileTexture(materialPath);
     closeModal();
   };
+
   return (
     <div>
       <Button onClick={openModal} type="button">
@@ -37,16 +41,23 @@ export const UploadButton = () => {
       </Button>
       <Modal isOpen={isModalOpen} closeModal={closeModal}>
         <div>
-          <h1>Selecciona el archivo que desea compartir </h1>
-
-          {materiales.map((material) => (
-            <div key={material.id}>
-              <Button onClick={() => selectMaterial(material.path)}>
-                <span>{material.nombre}</span>
-                {console.log(material)}
-              </Button>
+          <h1 className="text-lg font-semibold mb-4">
+            Selecciona el archivo que desea compartir
+          </h1>
+          {loading ? (
+            <div className="flex justify-center items-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500 mr-3"></div>
+              <p>Cargando materiales...</p>
             </div>
-          ))}
+          ) : (
+            materiales.map((material) => (
+              <div key={material.id} className="mb-2">
+                <Button onClick={() => selectMaterial(material.path)}>
+                  <span>{material.name}</span>
+                </Button>
+              </div>
+            ))
+          )}
         </div>
       </Modal>
     </div>
