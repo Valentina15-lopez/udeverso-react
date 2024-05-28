ac7bac3c8585f09e8f1efbc4b641dde7d1ba2e85
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-27 21:05:45 -0300

    agrego rol

--- client/src/components/Streaming/UploadButton.jsx
+++ client/src/components/Streaming/UploadButton.jsx
@@ -26,7 +26,7 @@ export const UploadButton = () => {
     };
 
     loadMaterials();
-  }, [userName]);
+  }, []);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
