4f38bfa4b8a0a28e37e7358c6fa3303e72211daf
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-28 14:28:34 -0300

    merge

--- client/src/components/Streaming/ChatInput.jsx
+++ client/src/components/Streaming/ChatInput.jsx
@@ -30,7 +30,7 @@ export const ChatInput = () => {
             className="bg-rose-400 p-2 ml-2 rounded-lg text-lg hover:bg-rose-600 text-white"
             isLight={false}
           >
-            Send
+            Enviar
           </Button>
         </div>
       </form>
