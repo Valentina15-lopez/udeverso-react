6eb59a30c4c2c81d0dac4da2430877392fe908a6
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-29 14:47:01 -0300

    merge

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,27 +1,19 @@
 const path = require("path");
 
-module.exports = function override(config, env) {
-  // Agrega el alias para importar pdfjs-dist
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    "pdfjs-dist": path.resolve(
-      __dirname,
-      "node_modules/pdfjs-dist/legacy/build/pdf.js"
-    ),
-  };
-
-  // Permitir la importación de pdf.worker
-  config.module.rules.push({
-    test: /pdf\.worker\.(min\.)?js/,
-    use: [
-      {
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
+// Agrega el alias para importar pdfjs-dis
+module.exports = {
+  webpack: function (config) {
+    config = {
+      resolve: {
+        ...config.resolve,
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        alias: {
+          "pdfjs-dist": path.resolve(
+            "./node_modules/pdfjs-dist/legacy/build/pdf.js"
+          ),
         },
       },
-    ],
-  });
-
-  return config;
+    };
+    return config;
+  },
 };
