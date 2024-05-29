4e2717af92a835d43b328783a85c0be84b1929ca
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-29 17:36:06 -0300

    merge

--- client/src/components/Streaming/PDFView.jsx
+++ client/src/components/Streaming/PDFView.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import pdfjs from "pdfjs-dist";
 
+pdfjs.GlobalWorkerOptions.workerSrc =
+  window.location.origin + "/pdf.worker.min.js";
+
 export const PDFView = ({ file }) => {
   const [loading, setLoading] = useState(false);
   const canvasRef = useRef(null);
