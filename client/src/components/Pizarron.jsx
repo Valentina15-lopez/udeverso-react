707ac60b51c3bbb0132e72349697fe6b546fb0c2
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-29 11:50:54 -0300

    merge

--- client/src/components/Pizarron.jsx
+++ client/src/components/Pizarron.jsx
@@ -24,7 +24,7 @@ export function Pizarron(props) {
         );
         const materials = response.data;
 
-        console.log(materials);
+        console.log("materiales", materials);
 
         // Encuentra el material con nombre "fileTexture"
         const fileTextureMaterial = materials.find(
