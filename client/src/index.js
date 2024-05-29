a73ea1c8006ddb216a814fd49f17ffc1f5376977
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-29 15:59:54 -0300

    merge

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import { ContextProvider } from "../src/context/ContexProvider";
 import App from "./App";
 import pdfjs from "pdfjs-dist";
+import urlWorker from "./pdf.worker";
 
-pdfjs.GlobalWorkerOptions.workerSrc = "https://d2v5g.csb.app/pdf.worker.js";
+pdfjs.GlobalWorkerOptions.workerSrc = urlWorker;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ContextProvider>
