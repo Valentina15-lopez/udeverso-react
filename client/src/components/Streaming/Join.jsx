68c03082315272cb6f2dbfa7dcc48715a883b7ba
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-30 23:21:04 -0300

    merge

--- client/src/components/Streaming/Join.jsx
+++ client/src/components/Streaming/Join.jsx
@@ -1,11 +1,19 @@
 import { NameInput } from "../../common/Name";
 import { Button } from "../../common/Button";
 import { socket } from "../../context/ContexProvider";
+import { useNavigate } from "react-router-dom";
 
 export const Join = () => {
+  const navigate = useNavigate();
+
   const createRoom = () => {
     socket.emit("create-room");
   };
+  // Función para manejar la redirección a la página anterior
+  const handleGoBack = () => {
+    navigate(-1); // Redirige a la página anterior en el historial
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="max-w-md w-full overflow-hidden my-8 bg-white shadow-lg rounded-lg p-6">
@@ -16,6 +24,7 @@ export const Join = () => {
         <Button onClick={createRoom} isLight={false}>
           Crear sala
         </Button>
+        <Button onClick={handleGoBack}>Volver al inicio</Button>
       </div>
     </div>
   );
