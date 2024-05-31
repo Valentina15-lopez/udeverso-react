6030a17e371c3cdde8d800df3d3a41042d23c612
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-31 00:55:16 -0300

    merge

--- client/src/pages/NotFound/index.jsx
+++ client/src/pages/NotFound/index.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>PAGINA NO ENCONTRADA</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full overflow-hidden my-8 bg-white shadow-lg rounded-lg p-6">
+        <h1 className="text-2xl font-semibold text-center mb-4 text-red-500">
+          PAGINA NO ENCONTRADA
+        </h1>
+      </div>
     </div>
   );
 };
