c3bd06be2374bd866063bbe82ed6d46cc3a498ba
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-30 23:39:21 -0300

    merge

--- client/src/common/Name.jsx
+++ client/src/common/Name.jsx
@@ -13,7 +13,7 @@ export const NameInput = () => {
 
   return (
     <input
-      className="w-full py-2 px-4 mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+      className="w-full py-2 px-4 mb-4 text-center border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
       placeholder="Ingrese su nombre"
       onChange={handleInputChange}
       value={userName}
--- client/src/components/Streaming/Join.jsx
+++ client/src/components/Streaming/Join.jsx
@@ -17,7 +17,7 @@ export const Join = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="max-w-md w-full overflow-hidden my-8 bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-2xl font-semibold text-center py-4 bg-gray-800 text-white">
+        <h2 className="text-2xl font-semibold text-center py-4 bg-gray-800 text-white mb-4">
           Crear una Sala
         </h2>
         <NameInput />
