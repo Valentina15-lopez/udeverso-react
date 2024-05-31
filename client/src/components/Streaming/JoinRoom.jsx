a371aeb01c5b0ae0c81bfea0d11c7c5a0f88ade1
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-30 23:07:20 -0300

    merge

--- client/src/components/Streaming/JoinRoom.jsx
+++ client/src/components/Streaming/JoinRoom.jsx
@@ -16,17 +16,21 @@ export const JoinRoom = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">Unirse a una Sala</h1>
-      <NameInput />
+      <div className="w-80">
+        <NameInput />
+      </div>
       <input
         type="text"
         value={roomId}
         onChange={(e) => setRoomId(e.target.value)}
         placeholder="Ingrese el ID de la Sala"
-        className="mb-4 p-2 border border-gray-300 rounded"
+        className="mb-4 p-2 border w-80 h-32 border-gray-300 rounded"
       />
-      <Button onClick={joinRoom} isLight={false}>
-        Unirse a la Sala
-      </Button>
+      <div className="w-80">
+        <Button onClick={joinRoom} isLight={false}>
+          Unirse a la Sala
+        </Button>
+      </div>
     </div>
   );
 };
