d8a64cc08c8ecc4efaf2f0641b09deaa6d687926
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-31 00:24:20 -0300

    merge

--- client/src/components/AvatarConfigPage.jsx
+++ client/src/components/AvatarConfigPage.jsx
@@ -78,7 +78,7 @@ export const AvatarConfigPage = () => {
           </label>
         </div>
         <Button onClick={handleSave}>Guardar Avatar</Button>
-        <div style={{ width: "300px", height: "300px" }}>
+        <div style={{ width: "100%", height: "100%" }}>
           <Canvas>
             <ambientLight />
             <OrbitControls />
