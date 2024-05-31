b89b532ac575b3c855e5435649cc6d3dc0481a0a
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-31 12:44:08 -0300

    merge

--- client/src/pages/AulaVirtual/index.jsx
+++ client/src/pages/AulaVirtual/index.jsx
@@ -34,7 +34,7 @@ const AulaVirtual = () => {
 
   // Función para manejar la redirección a la página anterior
   const handleGoBack = () => {
-    socket.emit("user-disconnected", { roomId });
+    socket.emit("disconnect");
     navigate(-1); // Redirige a la página anterior en el historial
   };
 
