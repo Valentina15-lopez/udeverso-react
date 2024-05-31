1a4522bf5cfc598a347bf148a2d12668f8b27fc9
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-31 00:59:22 -0300

    merge

--- client/src/pages/NotFound/index.jsx
+++ client/src/pages/NotFound/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../common/Button";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  // Función para manejar la redirección a la página anterior
+  const handleGoBack = () => {
+    navigate(-1); // Redirige a la página anterior en el historial
+  };
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="max-w-md w-full overflow-hidden my-8 bg-white shadow-lg rounded-lg p-6">
         <h1 className="text-2xl font-semibold text-center mb-4 text-red-500">
           PAGINA NO ENCONTRADA
         </h1>
+        <div className="flex flex-col gap-2">
+          <Button onClick={handleGoBack}>Volver al inicio</Button>
+        </div>
       </div>
     </div>
   );
