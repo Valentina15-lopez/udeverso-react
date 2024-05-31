96b53c594680ef3537dcc736634b46a341dc2356
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-30 23:30:33 -0300

    merge

--- client/src/common/Name.jsx
+++ client/src/common/Name.jsx
@@ -14,7 +14,7 @@ export const NameInput = () => {
   return (
     <input
       className="w-full py-2 px-4 mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-      placeholder="Enter your name"
+      placeholder="Ingrese su nombre"
       onChange={handleInputChange}
       value={userName}
     />
