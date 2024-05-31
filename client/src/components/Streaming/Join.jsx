ff28396cb5dd7965cc7f986f2609242708568cd0
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-30 23:19:31 -0300

    merge

--- client/src/components/Streaming/Join.jsx
+++ client/src/components/Streaming/Join.jsx
@@ -7,11 +7,16 @@ export const Join = () => {
     socket.emit("create-room");
   };
   return (
-    <div className="flex flex-col">
-      <NameInput />
-      <Button onClick={createRoom} isLight={false}>
-        Crear sala
-      </Button>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="max-w-md w-full overflow-hidden my-8 bg-white shadow-lg rounded-lg p-6">
+        <h2 className="text-2xl font-semibold text-center py-4 bg-gray-800 text-white">
+          Crear una Sala
+        </h2>
+        <NameInput />
+        <Button onClick={createRoom} isLight={false}>
+          Crear sala
+        </Button>
+      </div>
     </div>
   );
 };
