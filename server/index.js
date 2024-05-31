080539b59acad41a77cac85e37ad46cb8c2f06b5
Valentina15-lopez <valentina.lopez@despegar.com>
2024-05-31 18:09:27 -0300

    merge

--- server/index.js
+++ server/index.js
@@ -85,7 +85,6 @@ const roomHandler = (socket) => {
     if (!chats[roomId]) chats[roomId] = [];
     socket.emit("get-messages", chats[roomId]);
     console.log("user joined the room", roomId, peerId, userName);
-
     rooms[roomId][peerId] = { peerId, userName };
     socket.join(roomId);
     socket.to(roomId).emit("user-joined", { peerId, userName });
@@ -93,7 +92,6 @@ const roomHandler = (socket) => {
       roomId,
       participants: rooms[roomId],
     });
-    socket.emit("room-joined", { roomId });
     usersList.push({
       id: peerId,
       position: generateRandomPosition(),
