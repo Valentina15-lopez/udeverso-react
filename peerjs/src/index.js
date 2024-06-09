08b26553e78a720eef000e40ea3160e3d39d2b16
Mathias Nieres <mathiasnieres@gmail.com>
2024-06-09 00:58:08 -0300

    fix. mejoras

--- peerjs/src/index.js
+++ peerjs/src/index.js
@@ -4,6 +4,7 @@ import fs from "fs";
 const peerServer = PeerServer({
   port: 9000,
   path: "/",
+  debug: 2, // Cambia este valor según el nivel de logs que desees
   ssl: {
     key: fs.readFileSync("privkey.pem", "utf8"),
     cert: fs.readFileSync("fullchain.pem", "utf8"),
