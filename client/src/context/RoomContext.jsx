20e01db784ff59d04a79198b76bce119cc26e885
Mathias Nieres <mathiasnieres@gmail.com>
2024-06-09 01:58:33 -0300

    fix. mejoras

--- client/src/context/RoomContext.jsx
+++ client/src/context/RoomContext.jsx
@@ -96,9 +96,10 @@ export const RoomProvider = ({ children }) => {
 
   useEffect(() => {
     const peer = new Peer(userId, {
-      host: "metaversoude2.ddns.net",
+      host: "localhost",
       port: 9000,
       path: "/",
+      secure: true,
     });
 
     peer.on('open', () => {
