77ce7b1b5637ecd7ba46d2113f36148a195f163a
Valentina15-lopez <valentina.lopez@despegar.com>
2024-06-23 10:43:37 -0300

    worker

--- client/src/components/Streaming/PDFView.jsx
+++ client/src/components/Streaming/PDFView.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import * as pdfjsLib from 'pdfjs-dist/build/pdf';
-
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  window.location.origin + "/pdf.worker.min.js";
+import * as pdfjsLib from "pdfjs-dist/build/pdf";
 
 export const PDFView = ({ file }) => {
   const [loading, setLoading] = useState(false);
