879cf668114aba38d1cd46f20a0210ae27db9b29
Valentina15-lopez <valentina.lopez@despegar.com>
2024-07-19 12:33:56 -0300

    username

--- client/src/context/AvatarConfigContext.jsx
+++ client/src/context/AvatarConfigContext.jsx
@@ -11,6 +11,8 @@ export const AvatarConfigProvider = ({ children }) => {
   });
 
   const [saveAvatar, setSaveAvatar] = useState(false);
+
+  console.log(avatarConfig.userId);
   return (
     <AvatarConfigContext.Provider
       value={{ avatarConfig, setAvatarConfig, saveAvatar, setSaveAvatar }}
