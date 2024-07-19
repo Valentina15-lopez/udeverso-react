80da6c6bd341a9210fb6063fa73bffba76687905
Valentina15-lopez <valentina.lopez@despegar.com>
2024-07-19 11:25:39 -0300

    contex

--- client/src/context/AvatarConfigContext.jsx
+++ client/src/context/AvatarConfigContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { UserContext } from "../../context/UserContext";
+import { UserContext } from "../context/UserContext";
 
 export const AvatarConfigContext = createContext();
 
