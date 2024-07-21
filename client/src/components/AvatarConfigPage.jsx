cd8bf7ef493a1b4cc8a35f73b36e0e4dafc4eafc
Valentina15-lopez <valentina.lopez@despegar.com>
2024-07-21 10:35:01 -0300

    intento mandar config avatar al server

--- client/src/components/AvatarConfigPage.jsx
+++ client/src/components/AvatarConfigPage.jsx
@@ -50,7 +50,7 @@ export const AvatarConfigPage = () => {
   const fetchAvatarConfig = async () => {
     try {
       await axios.post(
-        "https://metaversoude2.ddns.net:3001/api/users",
+        `https://metaversoude2.ddns.net:3001/api/updateAvatar/${userName}/${users}`,
         avatarConfig
       );
     } catch (error) {
