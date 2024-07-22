7d1e034c24484aa214044c7f280aaec4cd4697a2
Valentina15-lopez <valentina.lopez@despegar.com>
2024-07-22 15:04:13 -0300

    disconnect

--- client/src/pages/AulaVirtual/index.jsx
+++ client/src/pages/AulaVirtual/index.jsx
@@ -37,10 +37,9 @@ const AulaVirtual = () => {
 
   // Función para manejar la redirección a la página anterior
   const handleGoBack = () => {
-    navigate(-1); // Redirige a la página anterior en el historial
-    socket.emit("user-disconnected", {
-      roomId,
-      peerId: userId,
+    socket.emit("user-disconnected", { roomId, peerId: userId }, () => {
+      // Redirigir a la página anterior en el historial después de manejar la desconexión
+      navigate(-1);
     });
   };
 
